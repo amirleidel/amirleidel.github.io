4f0a5f411c8612d2f79142793ac5357b735098bf
amirleidel <amir.leidel@stud.uni-heidelberg.de>
2024-12-27 01:46:33 +0100

    add favicon.ico

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,6 +18,11 @@ TIMEZONE = 'Europe/Rome'
 
 DEFAULT_LANG = 'en'
 
+STATIC_PATHS = [
+    'extra', 
+]
+EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'},}
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
